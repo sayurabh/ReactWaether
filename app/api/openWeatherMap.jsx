6cabec32bdf32a75bbe4f054ed18c4336bb0f4ba
sayurabh <sayurabh@gmail.com>
2017-10-28 07:24:12 +0530

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+
+
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=cc5635751b0dfe30e753131af95ffc89&units=imperial';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+	   
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
---
+++ app/components/about.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+//var About = React.createClass({
+//	render:function(){
+//		return (
+//			<h2>About Componenet</h2>
+//		)
+//	}
+//});
+var About = (props) =>{
+	return(
+		<h3>About Componenet</h3>
+	)
+};
+module.exports = About;
